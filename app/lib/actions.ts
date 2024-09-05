1091be3fbca2b2dad0d1a2eb5f698d3e7b65a89b
YogaLee12 <s631693747@126.com>
2024-09-06 05:21:45 +1000

    register function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,3 +29,67 @@ export async function authenticate(
         throw error;
     }
 }
+
+
+export async function register(
+// prevSate:string | undefined,
+    formData: FormData,
+    ) {
+const FormStu = z.object({
+    id:z.string(),
+    name:z.string(),
+    email:z.string(),
+    password: z.string().min(6),
+});
+
+const RegisterStu = FormStu.omit({id:true});
+
+    // get form data
+    const validatedStu = RegisterStu.safeParse( {
+        name: formData.get('name'),
+        email: formData.get('email'),
+        password: formData.get('password'),
+        });
+
+    // valid form fields
+    if (!validatedStu.success){
+        return {
+            errors: validatedStu.error.flatten().fieldErrors,
+            message: 'All fields are required.',
+        };
+    }
+
+    const {name, email,password} = validatedStu.data;
+    try {
+        const exsitingStu = await sql`
+            SELECT * FROM students WHERE email = ${email};
+        `
+        
+        if (exsitingStu.rows.length>0){
+            return {message:'Email already registered.'};
+            
+        }else{
+            const hashedPassword = await bcrypt.hash(password, 10);
+            try {
+                await sql`
+                INSERT INTO students (name , email, password)
+                VALUES (${name}, ${email}, ${hashedPassword})
+                `;
+            } catch (error) {
+                // If a database error occurs, return a more specific error.
+                return {
+                message: 'Database Error: Failed to Create Invoice.',
+                };
+            }
+
+    }
+        
+
+    } catch (error) {
+        console.error('Database Error',error);
+        return{message:'Internal server error'};
+    }
+    revalidatePath('/login');
+    redirect('/login');
+
+}
