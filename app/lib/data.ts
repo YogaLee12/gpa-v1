047e77e9bd1785bee32bd7d5aa3bdefa5e8d620a
YogaLee12 <s631693747@126.com>
2024-09-06 06:22:20 +1000

    fetch enrollments by current user

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,87 +1,92 @@
 import { sql } from '@vercel/postgres';
 import {
-  CustomerField,
-  CustomersTableType,
-  InvoiceForm,
-  InvoicesTable,
-  LatestInvoiceRaw,
-  Revenue,
-  LatestEnrollments,
+    CustomerField,
+    CustomersTableType,
+    InvoiceForm,
+    InvoicesTable,
+    LatestInvoiceRaw,
+    Revenue,
+    LatestEnrollments,
 } from './definitions';
+import { auth } from '@/auth';
 import { formatCurrency } from './utils';
 
-export async function fetchRevenue() {
-  try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
-}
-
-
-
 export async function fetchLatestEnrollments() {
-  try {
-      const data = await sql<LatestEnrollments>`
-      SELECT c.course_name, c.course_code 
-      FROM enrollments e
-      JOIN courses c ON e.course_id = c.id
-      ORDER BY e.year DESC
-      LIMIT 4`;
+    const stu = await auth();
+    try {
+        const data = await sql<LatestEnrollments>`
+        SELECT c.course_name, c.course_code 
+        FROM enrollments e
+        JOIN courses c ON e.course_id = c.id
+        JOIN students s ON s.id = e.user_id
+        WHERE s.id = ${stu?.user?.id}
+        ORDER BY e.year DESC
+        LIMIT 4`;
 
-      return data.rows;
-  } catch (error) {
-      console.error('Database Error:', error);
-      throw new Error('Failed to fetch the latest enrollments.');
-  }
+        return data.rows;
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch the latest enrollments.');
+    }
 }
-export async function fetchLatestInvoices() {
-  try {
-    const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+export async function fetchRevenue() {
+    try {
+        // Artificially delay a response for demo purposes.
+        // Don't do this in production :)
 
-    const latestInvoices = data.rows.map((invoice) => ({
-      ...invoice,
-      amount: formatCurrency(invoice.amount),
-    }));
-    return latestInvoices;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch the latest invoices.');
-  }
+        // console.log('Fetching revenue data...');
+        // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+        const data = await sql<Revenue>`SELECT * FROM revenue`;
+
+        // console.log('Data fetch completed after 3 seconds.');
+
+        return data.rows;
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch revenue data.');
+    }
+}
+
+
+
+
+export async function fetchLatestInvoices() {
+    try {
+        const data = await sql<LatestInvoiceRaw>`
+        SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+        FROM invoices
+        JOIN customers ON invoices.customer_id = customers.id
+        ORDER BY invoices.date DESC
+        LIMIT 5`;
+
+        const latestInvoices = data.rows.map((invoice) => ({
+        ...invoice,
+        amount: formatCurrency(invoice.amount),
+        }));
+        return latestInvoices;
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch the latest invoices.');
+    }
 }
 
 export async function fetchCardData() {
-  try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-    const invoiceStatusPromise = sql`SELECT
-          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-          FROM invoices`;
+    try {
+        // You can probably combine these into a single SQL query
+        // However, we are intentionally splitting them to demonstrate
+        // how to initialize multiple queries in parallel with JS.
+        const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+        const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+        const invoiceStatusPromise = sql`SELECT
+            SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+            SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+            FROM invoices`;
 
-    const data = await Promise.all([
-      invoiceCountPromise,
-      customerCountPromise,
-      invoiceStatusPromise,
+        const data = await Promise.all([
+        invoiceCountPromise,
+        customerCountPromise,
+        invoiceStatusPromise,
     ]);
 
     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
@@ -90,15 +95,15 @@ export async function fetchCardData() {
     const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
 
     return {
-      numberOfCustomers,
-      numberOfInvoices,
-      totalPaidInvoices,
-      totalPendingInvoices,
-    };
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch card data.');
-  }
+        numberOfCustomers,
+        numberOfInvoices,
+        totalPaidInvoices,
+        totalPendingInvoices,
+        };
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch card data.');
+    }
 }
 
 const ITEMS_PER_PAGE = 6;
