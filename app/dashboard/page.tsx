444197c03f3984167eb93f1e03f38cb3c0b0a510
YogaLee12 <s631693747@126.com>
2024-09-06 05:29:04 +1000

    user name fetch error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,11 +7,11 @@ import { sth } from '@/app/lib/fetchUserName';
 
 export default async function Page(){
     const LatestEnrollments = await fetchLatestEnrollments();
-
+    const StuName = await sth?.user?.name;
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-            {/* Welcom  {sth?.user?.name} ! */}
+            Welcom  {StuName} !
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
             {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
